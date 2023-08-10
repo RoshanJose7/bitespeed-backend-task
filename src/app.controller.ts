486c627a4b8a9af49561d8cac60bb7d6d461cf9a
RoshanJose <roshanstark007@gmail.com>
2023-08-10 10:44:09 +0530

    Fix: App Controller /identify request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,15 @@
-import { Controller, Get, Query } from "@nestjs/common";
+import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+
 import { AppService } from "./app.service";
+import { IdentifyContactDto } from "./app.dto";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("identify")
-  identifyContacts(
-    @Query("email") email?: string,
-    @Query("phoneNumber") phoneNumber?: string,
-  ) {
-    return this.appService.identifyContacts({ email, phoneNumber });
+  @Post("identify")
+  @HttpCode(HttpStatus.ACCEPTED)
+  identifyContacts(@Body() identifyContactDto: IdentifyContactDto) {
+    return this.appService.identifyContacts(identifyContactDto);
   }
 }
